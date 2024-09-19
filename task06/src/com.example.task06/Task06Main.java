55b2bcc22be703780045b6a96601851ad5a76c3a
blankec <Komba-rus05@mail.ru>
2024-09-19 16:56:18 +0500

    Кочетков Вадим. Задание 6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,5 +1,8 @@
 package com.example.task06;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,8 +13,9 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] numbers = new int[] {a, b, c, d};
+        Arrays.sort(numbers);
+        return numbers[numbers.length -1];
     }
 
 }
